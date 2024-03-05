1fc5d20d4e2162a1cadbe8c044b6acbd88593722
lengoc-quang <139666607+lengoc-quang@users.noreply.github.com>
2024-03-05 22:27:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,16 @@
-#coding:utf-8
-
+import time
+import os
+import joblib
 import streamlit as st
 import google.generativeai as genai
-from markdown import Markdown
-from io import StringIO
+from dotenv import load_dotenv
 from pykeyboard import PyKeyboard
 from datetime import datetime
 import subprocess
-import os
+from io import StringIO
+from markdown import Markdown
 
 k=PyKeyboard()
-api_key=os.getenv("API_KEY")
-
-with st.sidebar:
-    st.markdown(
-        """
-       <style>
-       [data-testid="stSidebar"][aria-expanded="true"]{
-           min-width: 330px;
-           max-width: 330px;
-       }
-       """,
-        unsafe_allow_html=True,
-    )
-    if api_key=='YOUR_API_KEY' or api_key='':
-        api_key = st.text_input("API Key", key="api_key", type="password")
-        "[Get a Gemini API key](https://aistudio.google.com/app/apikey)"
-    st.image("icon.jpg")
-    st.title("Welcome back!")
-    if st.sidebar.button("➕ New chat"):
-        now = datetime.now()
-        now = now.strftime("[%d-%m-%Y %H:%M:%S] ")
-        f=open("history.log", "a", encoding='utf-8')
-        f.write("*****************************************************\n")
-        f.write(now + "Restarted process.\n")
-        f.write("*****************************************************\n")
-        f.close()
-        k.tap_key(k.function_keys[5])
-    if st.sidebar.button("Open chat history"):
-        subprocess.Popen("history.log", shell=True)
 
 def unmark_element(element, stream=None):
     if stream is None:
@@ -61,48 +33,171 @@ __md.stripTopLevelTags = False
 def unmark(text):
     return __md.convert(text)
 
-# Initialize Gemini-Pro 
-genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-pro')
+load_dotenv()
+GOOGLE_API_KEY=os.environ.get('API_KEY')
 
-# Gemini uses 'model' for assistant; Streamlit uses 'assistant'
-def role_to_streamlit(role):
-  if role == "model":
-    return "assistant"
-  else:
-    return role
+new_chat_id = f'{time.time()}'
+MODEL_ROLE = 'ai'
+AI_AVATAR_ICON = '✨'
 
-# Add a Gemini Chat history object to Streamlit session state
-if "chat" not in st.session_state:
-    st.session_state.chat = model.start_chat(history = [])
+# Create a data/ folder if it doesn't already exist
+try:
+    os.mkdir('data/')
+except:
+    # data/ folder already exists
+    pass
 
-# Display Form Title
-st.title("Welcome to AI Chatbot!")
-st.markdown("###### Based in Gemini Pro API")
+# Load past chats (if available)
+try:
+    past_chats: dict = joblib.load('data/past_chats_list')
+except:
+    past_chats = {}
 
-# Display chat messages from history above current input box
-for message in st.session_state.chat.history:
-    with st.chat_message(role_to_streamlit(message.role)):
-        st.markdown(message.parts[0].text)
-
-# Accept user's next message, add to context, resubmit context to Gemini
-if prompt := st.chat_input("Enter question here..."):
-    if api_key != '':
-        # Display user's last message
-        st.chat_message("user").markdown(prompt)
-        f=open('history.log', "a", encoding='utf-8')
-        f.write("You: " + unmark(prompt) +"\n\n")
-        f.close()
-    
-        # Send user entry to Gemini and read the response
-        response = st.session_state.chat.send_message(prompt) 
-        
-        # Display last 
-        with st.chat_message("assistant"):
-            st.markdown(response.text)
-            f=open('history.log', "a", encoding='utf-8')
-            f.write("Chatbot: "+unmark(response.text) + "\n\n")
-            f.close()
+# Sidebar allows a list of past chats
+with st.sidebar:
+    st.markdown(
+        """
+       <style>
+       [data-testid="stSidebar"][aria-expanded="true"]{
+           min-width: 330px;
+           max-width: 330px;
+       }
+       """,
+        unsafe_allow_html=True,
+    )
+    if GOOGLE_API_KEY != '':
+        genai.configure(api_key=GOOGLE_API_KEY)
     else:
-        st.warning('Missing API key', icon="⚠️")
-            
+        GOOGLE_API_KEY = st.text_input("API Key", key="api_key", type="password")
+        "[Get a Gemini API key](https://aistudio.google.com/app/apikey)"
+        genai.configure(api_key=GOOGLE_API_KEY)
+    st.image("icon.jpg")
+    st.title("Welcome back!")
+    if st.sidebar.button("↻  Reload chat", help="Reload page."):
+        now = datetime.now()
+        now = now.strftime("[%d-%m-%Y %H:%M:%S] ")
+        f=open("history.log", "a", encoding='utf-8')
+        f.write("*****************************************************\n")
+        f.write(now + "Reloaded chat.\n")
+        f.write("*****************************************************\n")
+        f.close()
+        k.tap_key(k.function_keys[5])
+        
+    st.write('### Previous chat')
+    if st.session_state.get('chat_id') is None:
+        st.session_state.chat_id = st.selectbox(
+            label='Pick a past chat',
+            options=[new_chat_id] + list(past_chats.keys()),
+            format_func=lambda x: past_chats.get(x, 'New Chat'),
+            placeholder='',
+        )
+    else:
+        # This will happen the first time AI response comes in
+        st.session_state.chat_id = st.selectbox(
+            label='Pick a past chat',
+            options=[new_chat_id, st.session_state.chat_id] + list(past_chats.keys()),
+            index=1,
+            format_func=lambda x: past_chats.get(x, 'New Chat' if x != st.session_state.chat_id else st.session_state.chat_title),
+            placeholder='',
+        )
+    if st.sidebar.button("Open logs", help="Open chat logs"):
+        subprocess.Popen("history.log", shell=True)
+    # Save new chats after a message has been sent to AI
+    # TODO: Give user a chance to name chat
+    st.session_state.chat_title = f'ChatSession-{st.session_state.chat_id}'
+
+st.title('Welcome to Chatbot AI!')
+st.markdown("###### Based in Gemini API")
+
+# Chat history (allows to ask multiple questions)
+try:
+    st.session_state.messages = joblib.load(
+        f'data/{st.session_state.chat_id}-st_messages'
+    )
+    st.session_state.gemini_history = joblib.load(
+        f'data/{st.session_state.chat_id}-gemini_messages'
+    )
+    print('old cache')
+except:
+    st.session_state.messages = []
+    st.session_state.gemini_history = []
+    print('new_cache made')
+st.session_state.model = genai.GenerativeModel('gemini-pro')
+st.session_state.chat = st.session_state.model.start_chat(
+    history=st.session_state.gemini_history,
+)
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(
+        name=message['role'],
+        avatar=message.get('avatar'),
+    ):
+        st.markdown(message['content'])
+
+# React to user input
+if prompt := st.chat_input('Enter question here...'):
+    # Save this as a chat for later
+    if st.session_state.chat_id not in past_chats.keys():
+        past_chats[st.session_state.chat_id] = st.session_state.chat_title
+        joblib.dump(past_chats, 'data/past_chats_list')
+    # Display user message in chat message container
+    with st.chat_message('user'):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append(
+        dict(
+            role='user',
+            content=prompt,
+        )
+    )
+    ## Send message to AI
+    response = st.session_state.chat.send_message(
+        prompt,
+        stream=True,
+    )
+    # Display assistant response in chat message container
+    with st.chat_message(
+        name=MODEL_ROLE,
+        avatar=AI_AVATAR_ICON,
+    ):
+        message_placeholder = st.empty()
+        full_response = ''
+        assistant_response = response
+        # Streams in a chunk at a time
+        for chunk in response:
+            # Simulate stream of chunk
+            # TODO: Chunk missing `text` if API stops mid-stream ("safety"?)
+            for ch in chunk.text.split(' '):
+                full_response += ch + ' '
+                time.sleep(0.005)
+                # Rewrites with a cursor at end
+                message_placeholder.write(full_response + '▌')
+        # Write full message with placeholder
+        message_placeholder.write(full_response)
+
+    # Display last 
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+        f=open('history.log', "a", encoding='utf-8')
+        f.write("Chatbot: "+unmark(response.text) + "\n\n")
+        f.close()
+
+    # Add assistant response to chat history
+    st.session_state.messages.append(
+        dict(
+            role=MODEL_ROLE,
+            content=st.session_state.chat.history[-1].parts[0].text,
+            avatar=AI_AVATAR_ICON,
+        )
+    )
+    st.session_state.gemini_history = st.session_state.chat.history
+    # Save to file
+    joblib.dump(
+        st.session_state.messages,
+        f'data/{st.session_state.chat_id}-st_messages',
+    )
+    joblib.dump(
+        st.session_state.gemini_history,
+        f'data/{st.session_state.chat_id}-gemini_messages',
+    )
