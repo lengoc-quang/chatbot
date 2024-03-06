dc24c2892d4ffec1b2af98ef9fd113d9527312fc
lengoc-quang <139666607+lengoc-quang@users.noreply.github.com>
2024-03-06 22:18:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-import time
-import os
-import joblib
+#coding:utf-8
+
 import streamlit as st
 import google.generativeai as genai
-from dotenv import load_dotenv
+from markdown import Markdown
+from io import StringIO
 from pykeyboard import PyKeyboard
 from datetime import datetime
 import subprocess
-from io import StringIO
-from markdown import Markdown
 
 k=PyKeyboard()
 
@@ -33,27 +31,6 @@ __md.stripTopLevelTags = False
 def unmark(text):
     return __md.convert(text)
 
-load_dotenv()
-GOOGLE_API_KEY=os.environ.get('API_KEY')
-
-new_chat_id = f'{time.time()}'
-MODEL_ROLE = 'ai'
-AI_AVATAR_ICON = '✨'
-
-# Create a data/ folder if it doesn't already exist
-try:
-    os.mkdir('data/')
-except:
-    # data/ folder already exists
-    pass
-
-# Load past chats (if available)
-try:
-    past_chats: dict = joblib.load('data/past_chats_list')
-except:
-    past_chats = {}
-
-# Sidebar allows a list of past chats
 with st.sidebar:
     st.markdown(
         """
@@ -65,12 +42,11 @@ with st.sidebar:
        """,
         unsafe_allow_html=True,
     )
-    if GOOGLE_API_KEY != '':
-        genai.configure(api_key=GOOGLE_API_KEY)
-    else:
-        GOOGLE_API_KEY = st.text_input("API Key", key="api_key", type="password")
-        "[Get a Gemini API key](https://aistudio.google.com/app/apikey)"
-        genai.configure(api_key=GOOGLE_API_KEY)
+    GOOGLE_API_KEY = st.text_input("API Key", key="api_key", type="password")
+    st.markdown("[Get a Gemini API key](https://aistudio.google.com/app/apikey)")
+    genai.configure(api_key=GOOGLE_API_KEY)
+        # Initialize Gemini-Pro 
+    model = genai.GenerativeModel('gemini-pro')
     st.image("icon.jpg")
     st.title("Welcome back!")
     if st.sidebar.button("↻  Reload chat", help="Reload page."):
@@ -78,126 +54,53 @@ with st.sidebar:
         now = now.strftime("[%d-%m-%Y %H:%M:%S] ")
         f=open("history.log", "a", encoding='utf-8')
         f.write("*****************************************************\n")
-        f.write(now + "Reloaded chat.\n")
+        f.write(now + "Restarted process.\n")
         f.write("*****************************************************\n")
         f.close()
-        k.tap_key(k.function_keys[5])
-        
-    st.write('### Previous chat')
-    if st.session_state.get('chat_id') is None:
-        st.session_state.chat_id = st.selectbox(
-            label='Pick a past chat',
-            options=[new_chat_id] + list(past_chats.keys()),
-            format_func=lambda x: past_chats.get(x, 'New Chat'),
-            placeholder='',
-        )
-    else:
-        # This will happen the first time AI response comes in
-        st.session_state.chat_id = st.selectbox(
-            label='Pick a past chat',
-            options=[new_chat_id, st.session_state.chat_id] + list(past_chats.keys()),
-            index=1,
-            format_func=lambda x: past_chats.get(x, 'New Chat' if x != st.session_state.chat_id else st.session_state.chat_title),
-            placeholder='',
-        )
-    if st.sidebar.button("Open logs", help="Open chat logs"):
-        subprocess.Popen("history.log", shell=True)
-    # Save new chats after a message has been sent to AI
-    # TODO: Give user a chance to name chat
-    st.session_state.chat_title = f'ChatSession-{st.session_state.chat_id}'
+        k.tap_key(k.function_keys[5]
+                  
+    # st.markdown("#### Previous chat")
+    # if st.sidebar.button("Open chat history", help="Open a log file that contain chat history."):
+    #     subprocess.Popen("history.log", shell=True)
 
-st.title('Welcome to Chatbot AI!')
-st.markdown("###### Based in Gemini API")
+# Gemini uses 'model' for assistant; Streamlit uses 'assistant'
+def role_to_streamlit(role):
+  if role == "model":
+    return "assistant"
+  else:
+    return role
 
-# Chat history (allows to ask multiple questions)
-try:
-    st.session_state.messages = joblib.load(
-        f'data/{st.session_state.chat_id}-st_messages'
-    )
-    st.session_state.gemini_history = joblib.load(
-        f'data/{st.session_state.chat_id}-gemini_messages'
-    )
-    print('old cache')
-except:
-    st.session_state.messages = []
-    st.session_state.gemini_history = []
-    print('new_cache made')
-st.session_state.model = genai.GenerativeModel('gemini-pro')
-st.session_state.chat = st.session_state.model.start_chat(
-    history=st.session_state.gemini_history,
-)
+# Add a Gemini Chat history object to Streamlit session state
+if "chat" not in st.session_state:
+    st.session_state.chat = model.start_chat(history = [])
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(
-        name=message['role'],
-        avatar=message.get('avatar'),
-    ):
-        st.markdown(message['content'])
+# Display Form Title
+st.title("Welcome to AI Chatbot!")
+st.markdown("###### Based in Gemini Pro API")
 
-# React to user input
-if prompt := st.chat_input('Enter question here...'):
-    # Save this as a chat for later
-    if st.session_state.chat_id not in past_chats.keys():
-        past_chats[st.session_state.chat_id] = st.session_state.chat_title
-        joblib.dump(past_chats, 'data/past_chats_list')
-    # Display user message in chat message container
-    with st.chat_message('user'):
-        st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append(
-        dict(
-            role='user',
-            content=prompt,
-        )
-    )
-    ## Send message to AI
-    response = st.session_state.chat.send_message(
-        prompt,
-        stream=True,
-    )
-    # Display assistant response in chat message container
-    with st.chat_message(
-        name=MODEL_ROLE,
-        avatar=AI_AVATAR_ICON,
-    ):
-        message_placeholder = st.empty()
-        full_response = ''
-        assistant_response = response
-        # Streams in a chunk at a time
-        for chunk in response:
-            # Simulate stream of chunk
-            # TODO: Chunk missing `text` if API stops mid-stream ("safety"?)
-            for ch in chunk.text.split(' '):
-                full_response += ch + ' '
-                time.sleep(0.005)
-                # Rewrites with a cursor at end
-                message_placeholder.write(full_response + '▌')
-        # Write full message with placeholder
-        message_placeholder.write(full_response)
+# Display chat messages from history above current input box
+for message in st.session_state.chat.history:
+    with st.chat_message(role_to_streamlit(message.role)):
+        st.markdown(message.parts[0].text)
 
+# Accept user's next message, add to context, resubmit context to Gemini
+if prompt := st.chat_input("Enter question here..."):
+    ms = [prompt]
+#    if file_uploader is not None:
+ #       for file_ in file_uploader:
+  #          ms.append(file_)
+    # Display user's last message
+    st.chat_message("user").markdown(prompt)
+    f=open('history.log', "a", encoding='utf-8')
+    f.write("You: " + unmark(prompt) +"\n\n")
+    f.close()
+
+    # Send user entry to Gemini and read the response
+    response = st.session_state.chat.send_message(prompt) 
+    
     # Display last 
     with st.chat_message("assistant"):
         st.markdown(response.text)
         f=open('history.log', "a", encoding='utf-8')
         f.write("Chatbot: "+unmark(response.text) + "\n\n")
         f.close()
-
-    # Add assistant response to chat history
-    st.session_state.messages.append(
-        dict(
-            role=MODEL_ROLE,
-            content=st.session_state.chat.history[-1].parts[0].text,
-            avatar=AI_AVATAR_ICON,
-        )
-    )
-    st.session_state.gemini_history = st.session_state.chat.history
-    # Save to file
-    joblib.dump(
-        st.session_state.messages,
-        f'data/{st.session_state.chat_id}-st_messages',
-    )
-    joblib.dump(
-        st.session_state.gemini_history,
-        f'data/{st.session_state.chat_id}-gemini_messages',
-    )
