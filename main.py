e65077f9b23ecabfc3ee4c52863efbdd0e79e12e
lengoc-quang <139666607+lengoc-quang@users.noreply.github.com>
2024-03-07 11:28:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ import streamlit as st
 import google.generativeai as genai
 from markdown import Markdown
 from io import StringIO
-# from pykeyboard import PyKeyboard
 from datetime import datetime
 import subprocess
-
-# k=PyKeyboard()
 
 def unmark_element(element, stream=None):
     if stream is None:
@@ -48,20 +45,7 @@ with st.sidebar:
     genai.configure(api_key=GOOGLE_API_KEY)
         # Initialize Gemini-Pro 
     model = genai.GenerativeModel('gemini-pro')
-#    st.image("icon.jpg")
-#    if st.sidebar.button("↻  Reload chat", help="Reload page."):
- #       now = datetime.now()
-  #      now = now.strftime("[%d-%m-%Y %H:%M:%S] ")
-   #     f=open("history.log", "a", encoding='utf-8')
-    #    f.write("*****************************************************\n")
-     #   f.write(now + "Restarted process.\n")
-      #  f.write("*****************************************************\n")
-       # f.close()
-        #k.tap_key(k.function_keys[5])
-                  
-    # st.markdown("#### Previous chat")
-    # if st.sidebar.button("Open chat history", help="Open a log file that contain chat history."):
-    #     subprocess.Popen("history.log", shell=True)
+
 
 # Gemini uses 'model' for assistant; Streamlit uses 'assistant'
 def role_to_streamlit(role):
@@ -85,22 +69,22 @@ for message in st.session_state.chat.history:
 
 # Accept user's next message, add to context, resubmit context to Gemini
 if prompt := st.chat_input("Enter question here..."):
-    ms = [prompt]
-#    if file_uploader is not None:
- #       for file_ in file_uploader:
-  #          ms.append(file_)
-    # Display user's last message
-    st.chat_message("user").markdown(prompt)
-    f=open('history.log', "a", encoding='utf-8')
-    f.write("You: " + unmark(prompt) +"\n\n")
-    f.close()
-
-    # Send user entry to Gemini and read the response
-    response = st.session_state.chat.send_message(prompt) 
-    
-    # Display last 
-    with st.chat_message("assistant"):
-        st.markdown(response.text)
+    if GOOGLE_API_KEY == '':
+        st.error("You must enter an API key!")
+    else:
+        ms = [prompt]
+        # Display user's last message
+        st.chat_message("user").markdown(prompt)
         f=open('history.log', "a", encoding='utf-8')
-        f.write("Chatbot: "+unmark(response.text) + "\n\n")
+        f.write("You: " + unmark(prompt) +"\n\n")
         f.close()
+    
+        # Send user entry to Gemini and read the response
+        response = st.session_state.chat.send_message(prompt) 
+        
+        # Display last 
+        with st.chat_message("assistant"):
+            st.markdown(response.text)
+            f=open('history.log', "a", encoding='utf-8')
+            f.write("Chatbot: "+unmark(response.text) + "\n\n")
+            f.close()
